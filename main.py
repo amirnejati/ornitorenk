6d5ea3d8dfe628e20b0d728ca055fb719194b96a
Amir Hassan Nejati <nejati@indside.sahab.ir>
2019-07-18 16:52:37 +0430

    slight changes

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from ornitorenk.start_agt import start_agent
-from ornitorenk.start_srv import start_server
+# from ornitorenk.start_agt import start_agent
+# from ornitorenk.start_srv import start_server
 
+from ornitorenk.utility import cronize_function
 
 if __name__ == '__main__':
     pass
